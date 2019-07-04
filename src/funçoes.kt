ab683be24932eaa75974b726ac610a96d58d5b71
Isabelle Matos <isabellematos.a@gmail.com>
2019-07-03 22:20:17 -0300

    aprendendo o conceito de funções

---
+++ src/funçoes.kt
@@ -0,0 +1,32 @@
+//funções executam ações
+
+fun exibirMensagem (nome: String) {
+    //bloco de código
+    println ("você tem que comprar ração, $nome.")
+}
+//primeira forma de retornar, já completando o print na própria função
+fun somarNumeros (numero1: Int, numero2: Int): Int {
+    val totalNumeros = numero1 + numero2
+    println("O resultado de sua soma é $totalNumeros.")
+    return totalNumeros
+
+}
+
+//segunda forma de somar, retornando o total e tendo que armazenar em variavel, dentro da fun main
+fun somar (numero1: Int, numero2: Int): Int {
+    val total = numero1 + numero2
+    return total
+
+}
+
+fun main (args: Array<String>) {
+
+    exibirMensagem("Meredith")
+
+
+    somarNumeros(2,3)
+
+
+    val resultado = somar(6,7)
+    println("O resultado de sua soma é $resultado.")
+}
