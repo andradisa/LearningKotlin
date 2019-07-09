03d92222212659e0000e87c91413afedf5de4de7
Isabelle Matos <isabellematos.a@gmail.com>
2019-07-08 23:54:29 -0300

    praticando o conceito de classes e objetos

--- src/exercicios-classes2.kt
+++ src/exercicios-classes2.kt
@@ -1,6 +1,6 @@
 //entendendo o conceito de construtores
 
-class Peixe(var cor: String, var aguaSalgada: Boolean){
+class Peixe(var cor: String, var aguaSalgada: Boolean, var idade: Int){
 
     //caso não haja o var ali em cim, é necessário fazer init:
     /*
@@ -13,17 +13,29 @@ class Peixe(var cor: String, var aguaSalgada: Boolean){
         this.aguaSalgada = aguaSalgada
     }
     */
-    fun nadar(){
-        println("o peixinho está nadando! Sua cor é $cor e ele")
 
-        if (aguaSalgada == true) print("é um peixinho de água salgada!")
-        else print("não é um peixinho de água salgada.")
+    fun nadar(){
+        print("o peixinho está nadando! Sua cor é $cor e ele ")
+
+        if (aguaSalgada == true) println("é um peixinho de água salgada!")
+        else println("não é um peixinho de água salgada.")
+    }
+
+    fun mostrarIdade(){
+        if (idade < 3) {
+            println("dá pro caldo!")
+        } else if (idade < 5) {
+            println("idosinho mas a gente leva")
+        } else {
+            println("que que isso, já comprou o caixão?")
+        }
     }
 
 }
 
 fun main (args: Array<String>) {
 
-    val peixe1 = Peixe("azul", true)
+    val peixe1 = Peixe("azul", true, 5)
     peixe1.nadar()
+    peixe1.mostrarIdade()
 }
